6d02677e0faf6379c598c78a2e63d4a7067b12c1
Szymon Wysocki <szymek.wysocki@gmail.com>
2020-12-04 22:22:11 +0100

    Add option to load input as a single string

--- src/main/kotlin/days/Day.kt
+++ src/main/kotlin/days/Day.kt
@@ -9,12 +9,26 @@ abstract class Day(private val dayNumber: Int) {
     protected val input by lazy { getInput(sample = false) }
     protected val sampleInput by lazy { getInput(sample = true) }
 
+    protected val inputString by lazy { getInputString(sample = false) }
+    protected val sampleInputString by lazy { getInputString(sample = true) }
+
     private fun getInput(sample: Boolean = false): List<String> {
         val dir = if (sample) "samples" else "inputs"
         val path = "$dir/day_$dayNumber.txt"
         try {
             val file = File(javaClass.classLoader.getResource(path)!!.toURI())
             return file.readLines()
+        } catch (e: NullPointerException) {
+            throw FileNotFoundException("File $path doesn't exist")
+        }
+    }
+
+    private fun getInputString(sample: Boolean = false): String {
+        val dir = if (sample) "samples" else "inputs"
+        val path = "$dir/day_$dayNumber.txt"
+        try {
+            val file = File(javaClass.classLoader.getResource(path)!!.toURI())
+            return file.readText()
         } catch (e: NullPointerException) {
             throw FileNotFoundException("File $path doesn't exist")
         }
