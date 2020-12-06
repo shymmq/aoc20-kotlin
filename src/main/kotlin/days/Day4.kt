832ffd134f76f1e3077bef921f87696b8afeaa9f
Szymon Wysocki <szymek.wysocki@gmail.com>
2020-12-06 11:03:05 +0100

    (Day 4) Use regex for all fields

--- src/main/kotlin/days/Day4.kt
+++ src/main/kotlin/days/Day4.kt
@@ -4,32 +4,18 @@ class Day4 : Day(4) {
 
     private val passports = inputString.split("\n\n")
 
-    private val fields = mapOf<String, (String) -> Boolean>(
-        "byr" to { it.toIntOrNull() in 1920..2002 },
-        "iyr" to { it.toIntOrNull() in 2010..2020 },
-        "eyr" to { it.toIntOrNull() in 2020..2030 },
-        "hcl" to "#[0-9a-f]{6}".toRegex()::matches,
-        "ecl" to "amb|blu|brn|gry|grn|hzl|oth".toRegex()::matches,
-        "pid" to "[0-9]{9}".toRegex()::matches,
-        "hgt" to {
-            val hgt = it.filter(Char::isDigit).toIntOrNull()
-            when {
-                it.endsWith("cm") -> hgt in 150..193
-                it.endsWith("in") -> hgt in 59..76
-                else -> false
-            }
-        }
+    private val fields = mapOf<String, String>(
+        "byr" to "19[2-9][0-9]|200[0-2]",
+        "iyr" to "20(1[0-9]|20)",
+        "eyr" to "20(2[0-9]|30)",
+        "hcl" to "#[0-9a-f]{6}",
+        "ecl" to "amb|blu|brn|gry|grn|hzl|oth",
+        "pid" to "[0-9]{9}",
+        "hgt" to "1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in"
     )
 
 
     override fun partOne() = passports.count { fields.keys.all(it::contains) }
 
-
-    override fun partTwo() = passports.count { passport ->
-        fields.all { (field, isValid) ->
-            val pattern = "$field:([a-z0-9#]+)\\b".toRegex()
-            val value = pattern.find(passport)?.groupValues?.getOrNull(1)
-            value != null && isValid(value)
-        }
-    }
+    override fun partTwo() = passports.count { fields.map { (f, p) -> "$f:$p\\b".toRegex() }.all(it::contains) }
 }
